926d5409321460c0e352696207eb2a8b5ef43cf1
keval gandevia <gandeviakeval05@gmail.com>
2021-03-05 18:42:13 +0530

    bidding page is updated.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
   <link type="text/css" href="Assets/Files/CSS/home.css" rel="stylesheet">
 
-  <title>Hello, world!</title>
+  <title>Home Page</title>
 </head>
 
 <body>
