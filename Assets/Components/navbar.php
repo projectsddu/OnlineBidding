dc74382277880881709299c57f6262e27f1d0993
keval gandevia <gandeviakeval05@gmail.com>
2021-03-26 20:17:47 +0530

    minor fixes.

--- Assets/Components/navbar.php
+++ Assets/Components/navbar.php
@@ -1,5 +1,5 @@
 <?php
-// session_start();
+session_start();
     echo '<nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand text-white" href="index.php">OnlineBidding</a>
